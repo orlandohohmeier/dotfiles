734800dedaceba74b7e20cfe51db05d1d67906fe
Orlando Hohmeier <hello@orlandohohmeier.com>
2019-06-25 13:34:57 +0200

    feat: install python tools and libs

---
+++ 🐍.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ ############################################################################# 
+# 🐍 Install python libs and tools
+# -----------------------------------------------------------------------------
+
+pip install --user ansible
+pip install --user paramiko
